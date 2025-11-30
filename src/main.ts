f456a4cbea16471c0ac084e722495a31f8b7d637
Max Snischuk <m.snischuk@gmail.com>
2025-11-30 23:22:21 +0200

    feat: load env port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 4000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  await app.listen(port, () =>
+    console.log(`Application is listening on port: ${port}`),
+  );
 }
+
 bootstrap();
