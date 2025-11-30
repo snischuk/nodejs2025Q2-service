c4371ef49636c73c939ecd62f88d7a86a55c9baa
Max Snischuk <m.snischuk@gmail.com>
2025-11-30 23:24:37 +0200

    feat: add global pipe for validation inputs

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 const port = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(port, () =>
     console.log(`Application is listening on port: ${port}`),
