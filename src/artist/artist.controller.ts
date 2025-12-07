62f3ed08eafbef4e4c77a9d2929a59f025d7611b
Max Snischuk <m.snischuk@gmail.com>
2025-12-08 01:33:19 +0200

    fix: adjust 'artist' endpoint

--- src/artist/artist.controller.ts
+++ src/artist/artist.controller.ts
@@ -1,3 +1,52 @@
+// import {
+//   Body,
+//   Controller,
+//   Delete,
+//   Get,
+//   HttpCode,
+//   HttpStatus,
+//   Param,
+//   ParseUUIDPipe,
+//   Post,
+//   Put,
+// } from '@nestjs/common';
+// import { ArtistService } from './artist.service';
+// import { CreateArtistDto } from './dto/create-artist.dto';
+// import { UpdateArtistDto } from './dto/update-artist.dto';
+
+// @Controller('artist')
+// export class ArtistController {
+//   constructor(private readonly artistService: ArtistService) {}
+
+//   @Post()
+//   create(@Body() createArtistDto: CreateArtistDto) {
+//     return this.artistService.create(createArtistDto);
+//   }
+
+//   @Get()
+//   findAll() {
+//     return this.artistService.findAll();
+//   }
+
+//   @Get(':id')
+//   findOne(@Param('id', ParseUUIDPipe) id: string) {
+//     return this.artistService.findOne(id);
+//   }
+
+//   @Put(':id')
+//   update(
+//     @Param('id', ParseUUIDPipe) id: string,
+//     @Body() updateArtistDto: UpdateArtistDto,
+//   ) {
+//     return this.artistService.update(id, updateArtistDto);
+//   }
+
+//   @Delete(':id')
+//   @HttpCode(HttpStatus.NO_CONTENT)
+//   remove(@Param('id', ParseUUIDPipe) id: string) {
+//     return this.artistService.removeArtist(id);
+//   }
+// }
 import {
   Body,
   Controller,
@@ -19,31 +68,31 @@ export class ArtistController {
   constructor(private readonly artistService: ArtistService) {}
 
   @Post()
-  create(@Body() createArtistDto: CreateArtistDto) {
-    return this.artistService.create(createArtistDto);
+  async create(@Body() createArtistDto: CreateArtistDto) {
+    return await this.artistService.create(createArtistDto);
   }
 
   @Get()
-  findAll() {
-    return this.artistService.findAll();
+  async findAll() {
+    return await this.artistService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id', ParseUUIDPipe) id: string) {
-    return this.artistService.findOne(id);
+  async findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return await this.artistService.findOne(id);
   }
 
   @Put(':id')
-  update(
+  async update(
     @Param('id', ParseUUIDPipe) id: string,
     @Body() updateArtistDto: UpdateArtistDto,
   ) {
-    return this.artistService.update(id, updateArtistDto);
+    return await this.artistService.update(id, updateArtistDto);
   }
 
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
-  remove(@Param('id', ParseUUIDPipe) id: string) {
-    return this.artistService.removeArtist(id);
+  async remove(@Param('id', ParseUUIDPipe) id: string) {
+    await this.artistService.remove(id);
   }
 }
--- src/artist/artist.service.ts
+++ src/artist/artist.service.ts
@@ -1,28 +1,101 @@
+// import { Injectable, NotFoundException } from '@nestjs/common';
+// import { DatabaseService, DbEntity } from '../database/database.service';
+// import { Artist } from './entities/artist.entity';
+// import { UpdateArtistDto } from './dto/update-artist.dto';
+// import { CreateArtistDto } from './dto/create-artist.dto';
+// import { Album } from '../album/entities/album.entity';
+// import { Track } from '../track/entities/track.entity';
+
+// @Injectable()
+// export class ArtistService {
+//   constructor(private databaseService: DatabaseService) {}
+
+//   create(createArtistDto: CreateArtistDto): Artist {
+//     return this.databaseService.create<Artist>(
+//       DbEntity.ARTIST,
+//       createArtistDto,
+//     );
+//   }
+
+//   findAll(): Artist[] {
+//     return this.databaseService.findAll<Artist>(DbEntity.ARTIST);
+//   }
+
+//   findOne(id: string): Artist {
+//     const artist = this.databaseService.findOne<Artist>(DbEntity.ARTIST, id);
+
+//     if (!artist) {
+//       throw new NotFoundException('Artist not found');
+//     }
+
+//     return artist;
+//   }
+
+//   update(id: string, updateArtistDto: UpdateArtistDto): Artist {
+//     const artist = this.findOne(id);
+
+//     if (!artist) {
+//       throw new NotFoundException('Artist not found');
+//     }
+
+//     return this.databaseService.update<Artist>(
+//       DbEntity.ARTIST,
+//       id,
+//       updateArtistDto,
+//     );
+//   }
+
+//   removeArtist(id: string): void {
+//     const artist = this.findOne(id);
+
+//     if (!artist) {
+//       throw new NotFoundException('Artist not found');
+//     }
+
+//     this.databaseService.remove(DbEntity.ARTIST, id);
+
+//     this.databaseService.favorites.artists =
+//       this.databaseService.favorites.artists.filter(
+//         (artistId) => artistId !== id,
+//       );
+
+//     const artistAlbums = this.databaseService
+//       .findAll<Album>(DbEntity.ALBUM)
+//       .filter((album) => album.artistId === id);
+//     const artistTracks = this.databaseService
+//       .findAll<Track>(DbEntity.TRACK)
+//       .filter((track) => track.artistId === id);
+
+//     artistAlbums.forEach((album) => {
+//       album.artistId = null;
+//       this.databaseService.update(DbEntity.ALBUM, album.id, album);
+//     });
+//     artistTracks.forEach((track) => {
+//       track.artistId = null;
+//       this.databaseService.update(DbEntity.TRACK, track.id, track);
+//     });
+//   }
+// }
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { DatabaseService, DbEntity } from '../database/database.service';
-import { Artist } from './entities/artist.entity';
 import { UpdateArtistDto } from './dto/update-artist.dto';
 import { CreateArtistDto } from './dto/create-artist.dto';
-import { Album } from '../album/entities/album.entity';
-import { Track } from '../track/entities/track.entity';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { handleNotFoundException } from 'src/utils/utils';
 
 @Injectable()
 export class ArtistService {
-  constructor(private databaseService: DatabaseService) {}
+  constructor(private prisma: PrismaService) {}
 
-  create(createArtistDto: CreateArtistDto): Artist {
-    return this.databaseService.create<Artist>(
-      DbEntity.ARTIST,
-      createArtistDto,
-    );
+  async create(createArtistDto: CreateArtistDto) {
+    return await this.prisma.artist.create({ data: createArtistDto });
   }
 
-  findAll(): Artist[] {
-    return this.databaseService.findAll<Artist>(DbEntity.ARTIST);
+  async findAll() {
+    return await this.prisma.artist.findMany();
   }
 
-  findOne(id: string): Artist {
-    const artist = this.databaseService.findOne<Artist>(DbEntity.ARTIST, id);
+  async findOne(id: string) {
+    const artist = await this.prisma.artist.findUnique({ where: { id } });
 
     if (!artist) {
       throw new NotFoundException('Artist not found');
@@ -31,48 +104,22 @@ export class ArtistService {
     return artist;
   }
 
-  update(id: string, updateArtistDto: UpdateArtistDto): Artist {
-    const artist = this.findOne(id);
-
-    if (!artist) {
-      throw new NotFoundException('Artist not found');
+  async update(id: string, updateArtistDto: UpdateArtistDto) {
+    try {
+      return await this.prisma.artist.update({
+        where: { id },
+        data: updateArtistDto,
+      });
+    } catch (error) {
+      handleNotFoundException(error, 'Artist');
     }
-
-    return this.databaseService.update<Artist>(
-      DbEntity.ARTIST,
-      id,
-      updateArtistDto,
-    );
   }
 
-  removeArtist(id: string): void {
-    const artist = this.findOne(id);
-
-    if (!artist) {
-      throw new NotFoundException('Artist not found');
+  async remove(id: string) {
+    try {
+      return await this.prisma.artist.delete({ where: { id } });
+    } catch (error) {
+      handleNotFoundException(error, 'Artist');
     }
-
-    this.databaseService.remove(DbEntity.ARTIST, id);
-
-    this.databaseService.favorites.artists =
-      this.databaseService.favorites.artists.filter(
-        (artistId) => artistId !== id,
-      );
-
-    const artistAlbums = this.databaseService
-      .findAll<Album>(DbEntity.ALBUM)
-      .filter((album) => album.artistId === id);
-    const artistTracks = this.databaseService
-      .findAll<Track>(DbEntity.TRACK)
-      .filter((track) => track.artistId === id);
-
-    artistAlbums.forEach((album) => {
-      album.artistId = null;
-      this.databaseService.update(DbEntity.ALBUM, album.id, album);
-    });
-    artistTracks.forEach((track) => {
-      track.artistId = null;
-      this.databaseService.update(DbEntity.TRACK, track.id, track);
-    });
   }
 }
